2e7f2fc6266cd39a223c292ddc17cb4ad75082e3
Charles <C_VANZETTA@mutpoitiers.local>
2024-10-02 15:33:59 +0200

    Ajout de nouvelles modifications

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
 
-    $(".navbar").hide();
+$(".navbar").hide();
 
-    $(function() {
-        $(window).scroll(function() {
-
-            if ($(this).scrollTop() > 140) {
-                $('.navbar').fadeIn();
-            } else {
-              $('.navbar').fadeOut();
-            }
-        });
-    });
+$(function() {
+$(window).scroll(function() {
+if ($(this).scrollTop() > 140) {
+$('.navbar').fadeIn();
+} else {
+$('.navbar').fadeOut();
+}
+});
+});
 });
